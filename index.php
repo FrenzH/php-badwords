d04485ac3cf2af34711244f66ad0e74a81381604
FrenzH <franciattom99@gmail.com>
2022-10-12 16:30:19 +0100

    variables

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$paragraph='Hi, this is my first time using PHP , i hope it will be a success!';
+$lenght=strlen($paragraph);
+
+?>
+
+<?= $paragraph ?>
+<br />
+<?=  'paragraph lenght :' . $lenght ?>
